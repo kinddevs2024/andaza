573de3c6807d1c3a87edef2adcf87c31d2d9804f
uumarbek <koko87@mail.ru>
2024-10-13 00:33:38 +0500

    Analitik

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {  } from 'react'
 import './App.css'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
       <h1>
         Hello world
       </h1>
+      <Analytics/>
     </>
   )
 }
